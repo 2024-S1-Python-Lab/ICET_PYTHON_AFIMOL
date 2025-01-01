3f6f84652e85eed350b9b58656b742efd2c7e59f
afimolks <afimolks2003@gmail.com>
2025-01-01 11:27:42 +0530

    CO3 PROGRAM

---
+++ circle.py
@@ -0,0 +1,9 @@
+import math
+def area(r):
+    ar=math.pi*r**2
+    return ar
+
+def perimeter(r):
+    per=2.math.pi.*r
+    return per
+                      
---
+++ rectangle.py
@@ -0,0 +1,8 @@
+def area(length,breadth):
+    ar=length*breadth
+    return ar
+
+def perimeter(length,breadth):
+    perimeter=2*(length + breadth)
+    return perimeter
+                      
